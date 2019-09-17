d40a2c82f259a3cc1dd27d874cc670d520582c6a
Henry Proudhon <henry.proudhon@gmail.com>
2019-09-17 21:45:47 +0200

    formated for python 2 and 3

--- face-detection.py
+++ face-detection.py
@@ -11,12 +11,12 @@ myCamera = Camera(prop_set={'width':720, 'height': 1280})
 myDisplay = Display(resolution=(720, 1280))
 
 while not myDisplay.isDone():
-	frame = myCamera.getImage()
-	faces = frame.findHaarFeatures('face.xml')
-	if faces:
-		for face in faces:
-		print "Face at: " + str(face.coordinates())
-	else:
-		print "No faces detected."
-	   frame.save(myDisplay)
-	   sleep(.1)
+    frame = myCamera.getImage()
+    faces = frame.findHaarFeatures('face.xml')
+    if faces:
+        for face in faces:
+            print("Face at: " + str(face.coordinates()))
+    else:
+        print("No faces detected.")
+    frame.save(myDisplay)
+    sleep(.1)
