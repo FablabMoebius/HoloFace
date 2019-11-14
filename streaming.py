f9678cf971a785e259469dff9e7291426cf21bf0
Henry Proudhon <henry.proudhon@gmail.com>
2019-11-14 20:36:57 +0100

    new version of the streamline animation conforming to a regular grid

--- streaming.py
+++ streaming.py
@@ -7,6 +7,11 @@ from math import floor
 from animate import square
 
 class Stream():
+    """A stream has basically a length and a size function.
+
+    The stream can move acros the screen as time evolves. The position of the stream 
+    and its size function is used to compute the size of the blocks.
+    """
 
     STREAM_LENGTH = 31  # odd number of rows that spans a stream
     STREAM_LENGTH_STD = 10
@@ -22,7 +27,7 @@ class Stream():
         if length % 2 == 0:
             length += 1
         self.length = length
-        self.row = - (self.length - 1) / 2
+        self.position = - (self.length - 1) / 2 * SIZE
         if velocity is None:
             velocity = self.STREAM_VELOCITY
         self.velocity = velocity
@@ -30,8 +35,16 @@ class Stream():
         self.sizes = [self.size(i) for i in range(self.length)]
 
     def __repr__(self):
-        out = 'Stream of length %d at (%d, %d), Active=%s' % (self.length, self.col, self.row, self.active)
-        return out
+        out = ['Stream of length %d in column %d' % (self.length, self.col),
+               'position = %d pixels, velocity=%d pixel / s' % (self.position, self.velocity),
+               'active=%s' % self.active]
+        return "\n".join(out)
+
+    def get_head(self):
+        return self.position + self.length / 2 * SIZE
+        
+    def get_tail(self):
+        return self.position - self.length / 2 * SIZE
 
     @staticmethod
     def pick_length():
@@ -41,29 +54,31 @@ class Stream():
     def pick_velocity():
         return np.random.randint(Stream.STREAM_VELOCITY - Stream.STREAM_VELOCITY_STD, Stream.STREAM_VELOCITY + Stream.STREAM_VELOCITY_STD)
 
-    def size(self, i):
+    def size(self, row):
+        i = (row - self.position / SIZE) + self.length / 2
         # triangle
         #return 2 * np.abs(i / (STREAM_LENGTH - 1) - np.floor(i / (STREAM_LENGTH - 1) + 0.5)) * SIZE
         # piece-wise 3
         f = 0.735
         if i < self.length / 3:
-            return (i / (self.length / 3) ) * f * SIZE
+            return max((i / (self.length / 3) ) * f * SIZE, 1)
         elif i < 2 * self.length / 3:
             return f * SIZE
         else:
-            return (1 - (i - 2 * self.length / 3) / (self.length / 3)) * f * SIZE
+            return max((1 - (i - 2 * self.length / 3) / (self.length / 3)) * f * SIZE, 1)
 
     def blit(self, surface, color):
-        for i in range(self.length):
-            shape = square(self.sizes[i], color)
-            surface.blit(shape, (self.col * SIZE + SIZE / 2 - self.sizes[i] / 2, self.row - self.sizes[i] / 2 + (i - (self.length - 1) / 2) * SIZE))
+        for row in range(N_ROWS):
+            sz = self.size(row)
+            shape = square(sz, color)
+            surface.blit(shape, ((self.col + 0.5) * SIZE - sz / 2, (row + 0.5) * SIZE - sz / 2))
 
 N_COLS = 30
 N_ROWS = 30
-SIZE = 25
+SIZE = 25  # pixels
 DS = 0.5  # second, time sparating two streams
 FPS = 20
-save_screenshot = True
+save_screenshot = False
 
 def blit_shape(surface, rect, color):
     """
@@ -114,7 +129,7 @@ def streaming():
                     break
         t = pg.time.get_ticks()  # in ms
         screen.fill(marine)
-        '''
+        #'''
         xy = np.empty((2, N_ROWS, N_COLS), dtype=int)
         rec1 = square(1, white)
         for i in range(N_ROWS):
@@ -122,15 +137,15 @@ def streaming():
                 xy[0, i, j] = (2 * j + 1) * (SIZE / 2)
                 xy[1, i, j] = (2 * i + 1) * (SIZE / 2)
                 screen.blit(rec1, (xy[0, i, j], xy[1, i, j]))
-        '''
+        #'''
         # update stream positions
         dt = (t - last_t) / 1000
         for stream in streams:
         	if stream.active == True:
-        	    stream.row = stream.row + dt * stream.velocity
-        	    if stream.row > (N_ROWS + 0.5 * stream.length) * SIZE:
+        	    stream.position = stream.position + dt * stream.velocity  # pixels
+        	    if stream.get_tail() > N_ROWS * SIZE:
         	    	# deactivate this stream
-        	    	stream.row = - (stream.length - 1) / 2
+        	    	stream.position = - (stream.length - 1) / 2 * SIZE
         	    	stream.active = False
         	last_t = t
         # a new stream is activated every DS second
